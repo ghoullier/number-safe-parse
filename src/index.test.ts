e7a90aa908cea60082cd35452d8e3852e3808e91
Grégory Houllier <gregory.houllier@gmail.com>
2024-04-19 22:41:48 +0200

    feat: migrate from eslint to biome (#40)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,24 +1,24 @@
-import { numberSafeParse } from "./index"
+import { numberSafeParse } from "./index";
 
-import { describe, expect, test } from "bun:test"
+import { describe, expect, test } from "bun:test";
 
 describe("numberSafeParse()", () => {
-    test('Return `null` instead of `NaN` on failure (with matching type)', () => {
-        expect(numberSafeParse("yo")).toBe(null)
-    })
-    test('Does not parse things like "42foo" as "42" (vs useing`Number.parseFloat`)', () => {
-        expect(numberSafeParse('42foo')).toBe(null)
-    })
-    test('Does not parse whitespace - only strings as `0` (vs using`Number()`)', () => {
-        expect(numberSafeParse('     ')).toBe(null)
-    })
-    test('Does not parse "NaN" into "NaN" or "Infinity" into "Infinity"(vs using`Number()`)', () => {
-        expect(numberSafeParse("NaN")).toBe(null)
-        expect(numberSafeParse("Infinity")).toBe(null)
-        expect(numberSafeParse("-Infinity")).toBe(null)
-    })
-    test("Only parses token sequences that are *exactly* valid JS numbers", ()=> {
-        expect(numberSafeParse("12345")).toBe(12345)
-        expect(numberSafeParse("10.145")).toBe(10.145)
-    })
-})
+  test("Return `null` instead of `NaN` on failure (with matching type)", () => {
+    expect(numberSafeParse("yo")).toBe(null);
+  });
+  test('Does not parse things like "42foo" as "42" (vs useing`Number.parseFloat`)', () => {
+    expect(numberSafeParse("42foo")).toBe(null);
+  });
+  test("Does not parse whitespace - only strings as `0` (vs using`Number()`)", () => {
+    expect(numberSafeParse("     ")).toBe(null);
+  });
+  test('Does not parse "NaN" into "NaN" or "Infinity" into "Infinity"(vs using`Number()`)', () => {
+    expect(numberSafeParse("NaN")).toBe(null);
+    expect(numberSafeParse("Infinity")).toBe(null);
+    expect(numberSafeParse("-Infinity")).toBe(null);
+  });
+  test("Only parses token sequences that are *exactly* valid JS numbers", () => {
+    expect(numberSafeParse("12345")).toBe(12345);
+    expect(numberSafeParse("10.145")).toBe(10.145);
+  });
+});
